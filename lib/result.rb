5d9f87ce77ffb190b6086e0f9b205093a64e86f9
Dustin Barker <dustin.barker@gmail.com>
2010-05-05 05:51:50 +0800

    result won't need to fetch a URL anymore

--- lib/result.rb
+++ lib/result.rb
@@ -1,6 +1,5 @@
 class ItunesLinkMaker
   class Result
-    URL_ROOT = "http://ax.phobos.apple.com.edgesuite.net"
     
     attr_reader :name, :type
     
@@ -24,11 +23,6 @@ class ItunesLinkMaker
     def hash
       "#{@name}#{@type}#{@display_url}".hash
     end
-    
-  private
-    def get_url
-      doc = Hpricot(open(@display_url))
-      (doc/'textarea a').first['href']
-    end
+
   end
 end
